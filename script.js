a6cccb09d167d45a1466d6dc2aa34217084e7788
Pankaj Bhandari <pankajbhandari2010@gmail.com>
2025-05-23 12:25:37 +0530

    Practice

---
+++ script.js
@@ -0,0 +1,47 @@
+// let age = 15;
+// if (age > 18) {
+//     console.log("Adult");
+// }else {
+//     console.log("Minor");
+// }
+
+// console.log(age);
+
+// let username= window.prompt("Enter your name");
+// console.log(username);
+// let age = window.prompt("Enter your age");
+// console.log(age);
+// let isAdult = age >= 18;
+// if (isAdult) {
+//     console.log("Adult");
+// } else {
+//     console.log("Minor");
+// }   
+
+// let username ;
+// document.getElementById("submit").addEventListener("click", function() {
+//  username = document.getElementById("username").value;
+//  document.getElementById("welcome").textContent = "Hello " + username;
+// });
+
+// let age = window.prompt("Enter your age");
+
+// age = Number(age);
+// age = age + 1;
+// console.log(age, typeof age);
+
+// let x = "Hello";
+// let y = "Hello";
+// let z = "Hello";
+
+
+// y = string(y);
+// z = boolean(z);
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+const pi=3.14;
+let radius = window.prompt("Enter the radius of the circle");
+radius = Number(radius);    
+let area = pi * radius * radius;
