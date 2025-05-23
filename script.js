b98cd8f87ab5da3fe7d15a561ae151770a94c988
Pankaj Bhandari <pankajbhandari2010@gmail.com>
2025-05-23 17:10:44 +0530

    fix: add newline at end of file in script.js

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-// let age = 15;
-// if (age > 18) {
-//     console.log("Adult");
-// }else {
-//     console.log("Minor");
-// }
 
-// console.log(age);
-
-// let username= window.prompt("Enter your name");
-// console.log(username);
-// let age = window.prompt("Enter your age");
-// console.log(age);
-// let isAdult = age >= 18;
-// if (isAdult) {
-//     console.log("Adult");
-// } else {
-//     console.log("Minor");
-// }   
-
-// let username ;
-// document.getElementById("submit").addEventListener("click", function() {
-//  username = document.getElementById("username").value;
-//  document.getElementById("welcome").textContent = "Hello " + username;
-// });
-
-// let age = window.prompt("Enter your age");
-
-// age = Number(age);
-// age = age + 1;
-// console.log(age, typeof age);
-
-// let x = "Hello";
-// let y = "Hello";
-// let z = "Hello";
-
-
-// y = string(y);
-// z = boolean(z);
-// console.log(x, typeof x);
-// console.log(y, typeof y);
-// console.log(z, typeof z);
-
-const pi=3.14;
+const pi = 3.14;
 let radius = window.prompt("Enter the radius of the circle");
-radius = Number(radius);    
+radius = Number(radius);
 let area = pi * radius * radius;
+
+let circumference = 2 * pi * radius;
+console.log("The area of the circle is: " + area);
+console.log("The circumference of the circle is: " + circumference);
+let semiCircleArea = area / 2;
+console.log("The area of the semicircle is: " + semiCircleArea);
+let semiCircleCircumference = pi * radius + 2 * radius;
+console.log("The circumference of the semicircle is: " + semiCircleCircumference);
